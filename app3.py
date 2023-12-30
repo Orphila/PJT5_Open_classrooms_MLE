1607546d8337e90df3cf3dd4316529965599d281
Orphila <120557052+Orphila@users.noreply.github.com>
2023-12-30 09:26:51 +0100

    Changement de port

--- app3.py
+++ app3.py
@@ -225,5 +225,4 @@ def predict_endpoint():
 ################################################ ??
 
 if __name__ == '__main__':
-    #app.run(host="0.0.0.0",port=8081)
-    app.run(debug=True,port=8081)
+    app.run(host="0.0.0.0",port=80,debug=True)
