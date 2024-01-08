126c17fae2cbaff0689f7a2d2a34b0f1adc6e47f
Orphila ADJOVI <orphila_adjovi@carrefour.com>
2024-01-08 14:06:40 +0100

    upd front

--- front.py
+++ front.py
@@ -2,8 +2,8 @@ import streamlit as st
 import requests
     
 def get_prediction(text):
-    api_url = "https://tags-forecast.predict.onrender.com"
-    response = requests.get(api_url, params={'text': text},verify=False)
+    api_url = "https://tags-forecast.onrender.com"
+    response = requests.get(api_url, params={'text': text})
 
     if response.status_code == 200:
         result = response.json()
